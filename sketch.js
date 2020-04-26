e32fbcfc33d3a53b5bee56ea4335b6d7a2383574
Underreaper <suru.ganeshs@gmail.com>
2020-04-26 14:08:30 +0530

    Slingshot Game 2

--- sketch.js
+++ sketch.js
@@ -5,17 +5,12 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1,pig3;
-var backgroundImg,platform;
 var bird, slingshot;
 
 var score;
 
 var gameState = "onSling";
 
-function preload() {
-    //backgroundImg = loadImage("sprites/bg.png");
-    getBackgroundImg();
-}
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -24,60 +19,43 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    box1 = new Box(810,260,70,70);
+    box2 = new Box(810,260,70,70);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    log1 = new Log(810,320,300, PI/2);
+    log2 = new Log(810,240,300, PI/2);
 
-    log3 =  new Log(810,180,300, PI/2);
+    box3 = new Box(700,260,70,70);
+    box4 = new Box(920,260,70,70);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    box5 = new Box(700,260,70,70);
+    box6 = new Box(920,260,70,70);
 
-    bird = new Bird(200,50);
+    bird = new Bird(200,230);
 
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
+    slingshot = new SlingShot(bird.body,{x:200, y:230});
 }
 
 function draw(){
-if(backgroundImg){ 
-    background(backgroundImg);
-    noStroke();
-    textSize(35);
-    fill("white");
-    text("score" + score, width - 300, 50);
-}
+    background(205);
 
     Engine.update(engine);
     //strokeWeight(4);
-    box1.display();
-    box2.display();
+  
     ground.display();
-    pig1.display();
-    pig1.score();
     log1.display();
+    log2.display();
 
+    box1.display(); 
+    box2.display();
     box3.display();
     box4.display();
-    pig3.display();
-    pig3.score();
-    log3.display();
-
     box5.display();
-    log4.display();
-    log5.display();
-
+    box6.display();
+    
     bird.display();
-    platform.display();
-    //log6.display();
+
     slingshot.display();    
 }
 
@@ -101,19 +79,3 @@ function keyPressed(){
     }
 }
 
-async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
-    var responseJSON = await response.json();
-    var dateTime = responseJSON.datetime;
-    var hour = dateTime.slice(11,13);
-    console.log(hour);
-
-    if(hour >= 06 && hour <= 19){
-        bg = "sprites/bg.png";    
-    }
-    else{
-        bg = "sprites/bg2.jpg";
-    }
-    backgroundImg = loadImage(bg);
-    console.log(backgroundImg);
-}
