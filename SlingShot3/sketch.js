45d41dfc363f44d53a5c2f7dced93ad0216f4923
Suraj Ganesh <38537900+Underreaper@users.noreply.github.com>
2020-05-03 21:23:04 +0530

    Add files via upload

---
+++ SlingShot3/sketch.js
@@ -0,0 +1,97 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var bird, slingshot;
+
+var gameState = "onSling";
+
+var count = 0; 
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+
+    box1 = new Box(700,315,70,70);
+    box2 = new Box(700,350,70,70);
+    box3 = new Box(700,275,70,70);
+
+    log1 = new Log(810,320,300, PI/2);
+    log2 = new Log(810,280,300, PI/2);
+
+    box4 = new Box(810,315,70,70);
+    box5 = new Box(810,350,70,70);
+    box6 = new Box(810,275,70,70);
+
+    box7 = new Box(920,315,70,70);
+    box8 = new Box(920,350,70,70);
+    box9 = new Box(920,275,70,70);
+
+    bird = new Bird(200,230);
+
+    slingshot = new SlingShot(bird.body,{x:200, y:230});
+}
+
+function draw(){
+    background(205);
+    
+    textSize(25);
+    text("Score:" + count, 1050,50);
+
+    score();
+
+    Engine.update(engine);
+    //strokeWeight(4);
+  
+    ground.display();
+    log1.display();
+    log2.display();
+
+    box1.display(); 
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    
+    bird.display();
+
+    slingshot.display();
+
+}
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32 && bird.body.speed < 1){
+        bird.trajectory = [];
+        Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
+        slingshot.attach(bird.body);
+    }
+}
+
+function score(){
+    if(box1.body.speed > 3 || box2.body.speed > 3 || box3.body.speed > 3 || box4.body.speed > 3 ||box5.body.speed > 3 || box6.body.speed > 3 || box7.body.speed > 3 || box8.body.speed > 3 || box9.body.speed > 3){
+      count = count + 100
+    }
+}
