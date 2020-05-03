865b9e2f5d3f8d06ef049df3b5a544244a8797e6
Underreaper <suru.ganeshs@gmail.com>
2020-05-03 16:24:30 +0530

    Slingshot Game Part 3 Incomplete

--- sketch.js
+++ sketch.js
@@ -7,8 +7,6 @@ var engine, world;
 var box1, pig1,pig3;
 var bird, slingshot;
 
-var score;
-
 var gameState = "onSling";
 
 
@@ -20,17 +18,20 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
 
-    box1 = new Box(810,260,70,70);
-    box2 = new Box(810,260,70,70);
+    box1 = new Box(700,315,70,70);
+    box2 = new Box(700,350,70,70);
+    box3 = new Box(700,275,70,70);
 
     log1 = new Log(810,320,300, PI/2);
-    log2 = new Log(810,240,300, PI/2);
+    log2 = new Log(810,280,300, PI/2);
 
-    box3 = new Box(700,260,70,70);
-    box4 = new Box(920,260,70,70);
+    box4 = new Box(810,315,70,70);
+    box5 = new Box(810,350,70,70);
+    box6 = new Box(810,275,70,70);
 
-    box5 = new Box(700,260,70,70);
-    box6 = new Box(920,260,70,70);
+    box7 = new Box(920,315,70,70);
+    box8 = new Box(920,350,70,70);
+    box9 = new Box(920,275,70,70);
 
     bird = new Bird(200,230);
 
@@ -53,10 +54,14 @@ function draw(){
     box4.display();
     box5.display();
     box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
     
     bird.display();
 
-    slingshot.display();    
+    slingshot.display();
+
 }
 
 function mouseDragged(){
